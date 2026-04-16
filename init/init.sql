4c4c913f221691697abdaa2a43bd1cb58fc24edb
Aline <alineoliveiradesantana2013@gmail.com>
2026-04-16 14:36:22 -0300

    :boom: fix: Atualizar init.sql
    - colunas age_value e weight atualizadas respectivamente para integer e double

--- init/init.sql
+++ init/init.sql
@@ -25,14 +25,14 @@ DROP TABLE IF EXISTS `animal`;
 CREATE TABLE `animal` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `address_info` varchar(255) NOT NULL,
-  `age_value` varchar(255) DEFAULT NULL,
+  `age_value` integer DEFAULT NULL,
   `age_unit` varchar(255) DEFAULT NULL,
   `breed` varchar(255) NOT NULL,
   `created_at` datetime(6) NOT NULL,
   `name` varchar(255) NOT NULL,
   `sex` char(1) NOT NULL,
   `type` varchar(255) NOT NULL,
-  `weight` varchar(255) DEFAULT NULL,
+  `weight` double DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
